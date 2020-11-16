1da30cf0eea58da3a6d20cdd675099bffea32532
Dweej Patel <39783956+Dweej-Patel@users.noreply.github.com>
2020-11-16 00:32:10 -0500

    Scalable (#10)
    * made the app a little more scalable
    
    * ran migrations

---
+++ db_config.py
@@ -0,0 +1,7 @@
+import os
+
+DB_PASSWORD = os.environ["dbuser"]
+DB_HOST = os.environ["dbhost"]
+DB_PORT = 3306
+DB_USER = "admin"
+DB_NAME = "CatalogService"
---
+++ run.py
@@ -0,0 +1,8 @@
+from app import application
+
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+
+    application.run(host='0.0.0.0', port=8000)
